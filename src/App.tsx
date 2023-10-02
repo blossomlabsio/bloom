168b6d0ba38931639b80a447adc85fbc03f7d74f
A91y <65825207+A91y@users.noreply.github.com>
2023-10-02 15:51:50 +0530

    refactor: renamed main.tsx to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Footer } from './components/Footer';
-import { NavigationBar } from './components/NavigationBar';
+import { Footer } from './components/Footer/main';
+import { NavigationBar } from './components/NavigationBar/main';
 import './App.css';
-import { BloomMain } from './components/BloomMain';
+import { BloomMain } from './components/BloomMain/main';
 function App() {
   return (
     <div>
