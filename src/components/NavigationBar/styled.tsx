bed3c821292b5473734cb17ba1daf9c4e21c1db8
Shreya Prasad <shreyapd06@gmail.com>
2023-10-08 16:52:02 +0530

    style: basic enhancement of Navbar

--- src/components/NavigationBar/styled.tsx
+++ src/components/NavigationBar/styled.tsx
@@ -1,15 +1,33 @@
 import styled from 'styled-components';
 
 export const NavbarContainer = styled.nav`
-  background-color: #333;
+  background-color: #001d3d;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  padding: 0.5rem 0;
+  padding: 0.5rem 0 0.5rem 5rem;
   height: 3rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
   @media (max-width: 768px) {
     height: 2.5rem;
   }
+
+  @media (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
+export const NavList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  margin-left: auto;
+`;
+export const NavItem = styled.li`
+  margin: 0 10px;
 `;
 
 export const NavigationLink = styled.a`
@@ -20,6 +38,6 @@ export const NavigationLink = styled.a`
   transition: color 0.3s ease-in-out;
 
   &:hover {
-    color: #00bcd4;
+    color: #ffc300;
   }
 `;
