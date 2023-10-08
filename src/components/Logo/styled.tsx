cb7abaefbf438b8f4fcdf8df5afb7c826ac421bb
Shreya Prasad <shreyapd06@gmail.com>
2023-10-08 16:51:12 +0530

    feat: add Logo component

---
+++ src/components/Logo/styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const StyledLogo = styled.img`
+  width: 40px;
+`;
