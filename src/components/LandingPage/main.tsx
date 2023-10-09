0732cc785801ac3eddf39c0e4aee592606c82455
Shreya Prasad <shreyapd06@gmail.com>
2023-10-09 20:42:29 +0530

    chore: rename LandingPage to LandingPageBody

--- src/components/LandingPage/main.tsx
+++
@@ -1,9 +0,0 @@
-import { LandingPageComponent } from './styled';
-
-export const LandingPage = () => {
-  return (
-    <LandingPageComponent>
-      <h1>Bloom</h1>
-    </LandingPageComponent>
-  );
-};
