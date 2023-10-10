610e0448678ecf2a8736aba8d267b1d28539d6fc
Shreya Prasad <shreyapd06@gmail.com>
2023-10-11 00:26:05 +0530

    feat: redirect to /userprofile after successful login

---
+++ src/components/UserProfile/styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const UserProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+`;
