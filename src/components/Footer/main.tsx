f8afc0f432d46eb577e4900dab8adfc506ba600a
Shreya Prasad <shreyapd06@gmail.com>
2023-10-14 03:41:37 +0530

    refactor: improved footer UI

--- src/components/Footer/main.tsx
+++ src/components/Footer/main.tsx
@@ -1,9 +1,66 @@
-import { FooterContainer } from './styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'; // Import the specific icons you need
+import {
+  FooterWrapper,
+  FooterContent,
+  SocialMediaIconContainer,
+  SocialMediaIcon,
+  FooterLink,
+  List,
+  ListItem,
+  AllRightsReserved,
+  FooterHeader,
+} from './styled';
 
-export const Footer = () => {
+const FooterLinkData = [
+  {
+    category: 'Features',
+    links: { Creation: '#', Management: '#', Tracking: '#' },
+  },
+  {
+    category: 'Support',
+    links: { FAQ: '#', Feedback: '#', Contact: '#' },
+  },
+];
+
+const SocialMediaLinks = [
+  {
+    name: faGithub,
+    href: 'https://github.com/blossomlabsio',
+  },
+  {
+    name: faTwitter,
+    href: 'https://twitter.com',
+  },
+];
+
+const Footer = () => {
   return (
-    <FooterContainer>
-      <p>&copy; {new Date().getFullYear()} Blossomlabs. All rights reserved.</p>
-    </FooterContainer>
+    <FooterWrapper>
+      <FooterContent>
+        {FooterLinkData.map((categoryData, index) => (
+          <List key={index}>
+            <FooterHeader>{categoryData.category}</FooterHeader>
+            {Object.entries(categoryData.links).map(([name, href]) => (
+              <ListItem key={name}>
+                <FooterLink href={href}>{name}</FooterLink>
+              </ListItem>
+            ))}
+          </List>
+        ))}
+      </FooterContent>
+      <SocialMediaIconContainer>
+        <AllRightsReserved>
+          &copy; 2023 Bloom, All Rights Reserved.
+        </AllRightsReserved>
+        {SocialMediaLinks.map(({ name, href }) => (
+          <SocialMediaIcon key={href} href={href}>
+            <FontAwesomeIcon icon={name} />
+          </SocialMediaIcon>
+        ))}
+      </SocialMediaIconContainer>
+    </FooterWrapper>
   );
 };
+
+export default Footer;
--- src/components/Footer/styled.tsx
+++ src/components/Footer/styled.tsx
@@ -1,12 +1,66 @@
 import styled from 'styled-components';
 
-export const FooterContainer = styled.footer`
-  background-color: #333;
-  color: white;
-  padding: 0.5rem;
-  text-align: center;
-  font-size: 14px;
+export const FooterWrapper = styled.footer`
+  display: flex;
+  flex-flow: row wrap;
   position: absolute;
   bottom: 0;
-  width: 100%;
+  width: 100vw;
+  background-color: rgb(242, 149, 89);
+  color: #202c39;
+`;
+
+export const FooterContent = styled.div`
+  display: inline-flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  flex: 1 50%;
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const ListItem = styled.li`
+  margin-top: 0.5rem;
+`;
+
+export const FooterLink = styled.a`
+  text-decoration: none;
+  display: block;
+  color: #202c39;
+  cursor: pointer;
+
+  &:hover {
+    color: #fff;
+  }
+`;
+
+export const FooterHeader = styled.p`
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+
+export const SocialMediaIconContainer = styled.div`
+  display: inline-flex;
+  align-items: flex-start;
+  justify-content: end;
+  flex: 1 51%;
+  padding: 0 4rem 0.5rem 0;
+`;
+
+export const SocialMediaIcon = styled.a`
+  font-size: 1.5rem;
+  margin: 0px 0.6rem 0.3rem 0;
+  color: #202c39;
+  &:hover {
+    color: #fff;
+  }
+`;
+
+export const AllRightsReserved = styled.p`
+  margin: 0 auto 0 2rem;
+  font-size: 0.8rem;
+  font-weight: bold;
 `;
