37b4e7dd004a94231670c909beecaf7f8a4c2071
A91y <65825207+A91y@users.noreply.github.com>
2023-10-13 16:42:32 +0530

    fix: enhanced footer

--- src/components/Footer/main.tsx
+++ src/components/Footer/main.tsx
@@ -1,9 +1,51 @@
-import { FooterContainer } from './styled';
-
+import React from 'react';
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+  CopyRight,
+  SocialIcons,
+  CopyRightContainer,
+} from './styled';
+import { AiFillInstagram, AiFillTwitterSquare } from 'react-icons/ai';
 export const Footer = () => {
   return (
-    <FooterContainer>
-      <p>&copy; {new Date().getFullYear()} Blossomlabs. All rights reserved.</p>
-    </FooterContainer>
+    <Box>
+      {/* <h1 style={{ color: 'purple', textAlign: 'center', marginTop: '-50px' }}>
+        Bloom: The CFP Portal
+      </h1> */}
+      <Container>
+        <Row>
+          <Column>
+            <Heading>Features</Heading>
+            <FooterLink href="#">Creation</FooterLink>
+            <FooterLink href="#">Management</FooterLink>
+            <FooterLink href="#">Tracking</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Profile</Heading>
+            <FooterLink href="#">Edit</FooterLink>
+            <FooterLink href="#">View</FooterLink>
+            <FooterLink href="#">Settings</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Support</Heading>
+            <FooterLink href="#">FAQ</FooterLink>
+            <FooterLink href="#">Contact</FooterLink>
+            <FooterLink href="#">Feedback</FooterLink>
+          </Column>
+        </Row>
+      </Container>
+      <CopyRightContainer>
+        <CopyRight>© 2023 Bloom, All Rights Reserved.</CopyRight>
+        <SocialIcons>
+          <AiFillInstagram />
+          <AiFillTwitterSquare />
+        </SocialIcons>
+      </CopyRightContainer>
+    </Box>
   );
 };
--- src/components/Footer/styled.tsx
+++ src/components/Footer/styled.tsx
@@ -1,12 +1,80 @@
 import styled from 'styled-components';
 
-export const FooterContainer = styled.footer`
-  background-color: #333;
-  color: white;
-  padding: 0.5rem;
-  text-align: center;
-  font-size: 14px;
+export const Box = styled.div`
+  padding: 80px 60px;
+  background: #f29559;
   position: absolute;
   bottom: 0;
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1000px;
+  // margin: 0 auto;
+  /* background: red; */
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
+
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
+  grid-gap: 100px;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+export const FooterLink = styled.a`
+  color: #000;
+  margin-bottom: 20px;
+  font-size: 18px;
+  text-decoration: none;
+
+  &:hover {
+    color: white;
+    transition: 200ms ease-in;
+  }
+`;
+
+export const Heading = styled.p`
+  font-size: 24px;
+  color: #000;
+  margin-bottom: 40px;
+  font-weight: bold;
+`;
+
+export const CopyRight = styled.p`
+  color: #000;
+  // text-align: center;
+  margin-top: 20px;
+  margin-left: 60px;
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  // justify-content: space-between;
+  gap: 10px;
+  align-items: center;
+  width: 240px;
+`;
+
+export const CopyRightContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
 `;
